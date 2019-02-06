06834935cd840e6af6125df37e9a0b5412b5a3b0
Stephen Lead <stephen.lead@gmail.com>
2019-02-06 19:11:56 +1100

    add readme and comments

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # GeoServer_TypeAhead
 
-A demonstration site using GeoServer as the source of a type-ahead
+This is a demonstration site showing how to use GeoServer as the source of a type-ahead, based on [Twitter TypeAhead](https://twitter.github.io/typeahead.js/).
+
+The modifications to the Twitter demos are found in the `prepare` and `transform` functions:
+
+```
+// Initialize the Bloodhound engine
+var bloodhoundEngine = new Bloodhound({
+  datumTokenizer: function(datum) {
+      return Bloodhound.tokenizers.whitespace(datum.value);
+    },
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  limit: 15,
+  remote: {
+    url: typeaheadBase + '%QUERY',
+    wildcard: '%QUERY',
+    prepare: function(query, settings) {
+      // Build the query string in a GeoServer-ready format
+      settings.url = typeaheadBase + jQuery.param(typeaheadParams);
+      settings.url += "&CQL_FILTER=strToUpperCase(" + nameField + ")%20like%20%27QUERY%25%27";
+      settings.url = settings.url.replace('QUERY', query.toUpperCase());
+      return settings;
+    },
+    transform: function(response) {
+      // temporarily persist the geometries of the returned states
+      geometries = {};
+      return $.map(response.features, function(feature) {
+        geometries[feature.properties[nameField]] = feature.geometry;
+
+        // Return the state name to the TypeAhead suggestion dropdown
+        return (feature.properties[nameField]);
+      });
+    }
+  },
+});
+bloodhoundEngine.initialize();
+```
+
+See the demo at https://slead.github.io/GeoServer_TypeAhead/ which also shows how [Leaflet.js](https://leafletjs.com/) and [Tokenfield](http://sliptree.github.io/bootstrap-tokenfield) can be integrated.
+
+![screenshot](https://i.imgur.com/l3EbdaD.png)
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var typeaheadBase = "https://demo.geo-solutions.it/geoserver/topp/ows?";
 var queryLayer = "topp:states";
 var nameField = "STATE_NAME";
-var geometries = {};
-var mapGeometries = {};
+
+// For Tokenfield, maintain 2 lists of geometries in memory
+var geometries = {}; // returned geometries from the TypeAhead response
+var mapGeometries = {}; // geometries that are currently shown on the map
 
 var typeaheadParams = {
   service: "WFS",
@@ -12,8 +14,10 @@ var typeaheadParams = {
   typeName: queryLayer
 }
 
+// Create a simple map centred on the USA
 var map = L.map('map').setView([33.6132609,-97.6884029], 5);
 
+// Add the stamen grayscale basemap
 var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: 'abcd',
@@ -22,8 +26,10 @@ var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/to
   ext: 'png'
 }).addTo(map);
 
-var resultsLayer = L.geoJSON(null).addTo(map);
+// Create an empty layer to hold the state boundaries
+var statesLayer = L.geoJSON(null).addTo(map);
 
+// Initialize the Bloodhound engine
 var bloodhoundEngine = new Bloodhound({
   datumTokenizer: function(datum) {
       return Bloodhound.tokenizers.whitespace(datum.value);
@@ -34,43 +40,52 @@ var bloodhoundEngine = new Bloodhound({
     url: typeaheadBase + '%QUERY',
     wildcard: '%QUERY',
     prepare: function(query, settings) {
+      // Build the query string in a GeoServer-ready format
       settings.url = typeaheadBase + jQuery.param(typeaheadParams);
       settings.url += "&CQL_FILTER=strToUpperCase(" + nameField + ")%20like%20%27QUERY%25%27";
       settings.url = settings.url.replace('QUERY', query.toUpperCase());
       return settings;
     },
     transform: function(response) {
+      // temporarily persist the geometries of the returned states
       geometries = {};
       return $.map(response.features, function(feature) {
         geometries[feature.properties[nameField]] = feature.geometry;
+
+        // Return the state name to the TypeAhead suggestion dropdown
         return (feature.properties[nameField]);
       });
     }
   },
 });
-
 bloodhoundEngine.initialize();
+
+// Configure the states field for TokenField (http://sliptree.github.io/bootstrap-tokenfield)
 $('#txtStates').tokenfield({
   typeahead: [null, { source: bloodhoundEngine.ttAdapter() }]
 }).on('tokenfield:createtoken', function (e) {
+  // When a token is created, obtain its geometry from the temporary geometries object
   var geometry = geometries[e.attrs.value];
   mapGeometries[e.attrs.value] = geometry;
+
+  // Update the map to show the latest set of geometries
   displayGeometries();
-  geometries = {};
 }).on('tokenfield:removedtoken', function (e) {
+  // When a token is removed, also remove its geometry from the map
   delete mapGeometries[e.attrs.value];
   displayGeometries()
 
 });
 
 function displayGeometries(){
-  resultsLayer.clearLayers();
+  // Update the map to show the current set of tokens
+  statesLayer.clearLayers();
   for (var key in mapGeometries) {
     if (mapGeometries.hasOwnProperty(key)) {
-        var mapGeometry = mapGeometries[key];
-        if (mapGeometry){
-          resultsLayer.addData(mapGeometry);
-        }
+      var mapGeometry = mapGeometries[key];
+      if (mapGeometry){
+        statesLayer.addData(mapGeometry);
+      }
     }
   }
 }
