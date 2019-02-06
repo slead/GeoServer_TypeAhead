ee331c2a2644ed2328ebf5948f398e6b6460dea2
Stephen Lead <stephen.lead@gmail.com>
2019-02-06 14:23:32 +1100

    add results to the map, remove when removed from list

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 var typeaheadBase = "https://demo.geo-solutions.it/geoserver/topp/ows?";
 var queryLayer = "topp:states";
 var nameField = "STATE_NAME";
+var geometries = {};
+var mapGeometries = {};
 
 var typeaheadParams = {
-    service: "WFS",
-    version: "1.0.0",
-    request: "GetFeature",
-    outputFormat: "application/json",
-    typeName: queryLayer
-  }
+  service: "WFS",
+  version: "1.0.0",
+  request: "GetFeature",
+  outputFormat: "application/json",
+  typeName: queryLayer
+}
+
+var map = L.map('map').setView([33.6132609,-97.6884029], 5);
+
+var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
+
+var resultsLayer = L.geoJSON(null).addTo(map);
 
 var bloodhoundEngine = new Bloodhound({
   datumTokenizer: function(datum) {
@@ -26,8 +40,10 @@ var bloodhoundEngine = new Bloodhound({
       return settings;
     },
     transform: function(response) {
+      geometries = {};
       return $.map(response.features, function(feature) {
-        return toTitleCase(feature.properties[nameField]);
+        geometries[feature.properties[nameField]] = feature.geometry;
+        return (feature.properties[nameField]);
       });
     }
   },
@@ -36,8 +52,25 @@ var bloodhoundEngine = new Bloodhound({
 bloodhoundEngine.initialize();
 $('#txtStates').tokenfield({
   typeahead: [null, { source: bloodhoundEngine.ttAdapter() }]
+}).on('tokenfield:createtoken', function (e) {
+  var geometry = geometries[e.attrs.value];
+  mapGeometries[e.attrs.value] = geometry;
+  displayGeometries();
+  geometries = {};
+}).on('tokenfield:removedtoken', function (e) {
+  delete mapGeometries[e.attrs.value];
+  displayGeometries()
+
 });
 
-function toTitleCase(str){
-  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+function displayGeometries(){
+  resultsLayer.clearLayers();
+  for (var key in mapGeometries) {
+    if (mapGeometries.hasOwnProperty(key)) {
+        var mapGeometry = mapGeometries[key];
+        if (mapGeometry){
+          resultsLayer.addData(mapGeometry);
+        }
+    }
+  }
 }
